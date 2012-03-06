10d01663423bfb0c975edae1305027dc7e11fdeb
Vasu Thiyagarajan <srtvasu@gmail.com>
2012-03-05 23:46:18 -0500

    First Git Commit

---
+++ Scala_D1/src/main/java/scala/training/Upper.scala
@@ -0,0 +1,19 @@
+package scala.training
+
+object Upper{
+	
+	def upper(strings: String*): Seq[String] = {
+		strings.map( (s:String) => {
+				Console.println(s)
+				s.toUpperCase()
+		})
+	}
+	
+	def main(args: Array[String]) = {
+		//var up = new Upper
+		Console.print(Upper.upper("Vasu", "Shreenidhi", "Vidya"))
+		println("")
+	}
+
+}
+
